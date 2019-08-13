9ff5c10cb723fec044a5f5d14dc45feefda7fa5a
Unni Ramachandran <unnipbvr@gmail.com>
2019-08-13 13:56:55 -0400

    node js activities

--- Week10/01-Activities/26-Inquirer_Users/Unsolved/userprompt.js
+++ Week10/01-Activities/26-Inquirer_Users/Unsolved/userprompt.js
@@ -14,3 +14,36 @@
 // Remember to be creative!
 
 // ========================================================================
+var inquirer = require("inquirer");
+inquirer
+  .prompt([
+    // Here we create a basic text prompt.
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "username"
+    },
+ 
+    {
+        type:"password",
+        message: "Set your password",
+        name: "pass"
+    },
+    {
+        type:"list",
+        choices:["HTML", "CSS", "JQUERY"],
+        message: "What do you like most?",
+        name: "favTech"
+    },
+    {
+        type:"checkbox",
+        message:"Select your choices",
+        choices:[{name: "Ice cream"}, {name: "Milk shake", checked: true}],
+        name: "foodChoice"
+    }
+
+  ])
+  .then(function(inquirerResponse) {
+    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+    console.log(inquirerResponse)
+  });
